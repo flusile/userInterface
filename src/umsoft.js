257f8626fbbbe32688a2df6bf021b98d7c1180f4
Ulf Michaelis <ulf@vdr>
2011-09-05 21:27:01 +0200

    Platz für den ServerTimestamp gemacht und JS-Code angepaßt.

--- src/umsoft.js
+++ src/umsoft.js
@@ -1,7 +1,9 @@
+"use strict";
 // (c) Ulf Michaelis
 // enthält geklauten Code aus anderen Quellen
 // setzt jquery voraus
 var glob_lp;
+var glob_cnt = 0;
 //init_meter = function()
 //$(function()
 //{
@@ -10,52 +12,54 @@ var glob_lp;
 // und das deshalb Wärmewerte anzeigen kann.
 // Möglicherweise muß dafür ein eigener Gradient gebaut werden.
 
-      $("meter").each(function() 
-      {
-        var meter = $(this);
-        var label = $("<span>" + meter.html() + "</span>"); 
-        label.addClass("label");
+$("meter").each(function() 
+{
+  var meter = $(this);
+  var label = $("<span>" + meter.html() + "</span>"); 
+  label.addClass("label");
 
-        var fill = $("<div></div>"); 
-        fill.addClass("fill");
-        fill.css("width",(meter.attr("value") / meter.attr("max") * 100) + "%"); 
-        fill.append("<div style='clear:both;'><br></div>");
+  var fill = $("<div></div>"); 
+  fill.addClass("fill");
+  fill.css("width",(meter.attr("value") / meter.attr("max") * 100) + "%"); 
+  fill.append("<div style='clear:both;'><br></div>");
       
-        meter.html(""); 
-        meter.append(fill);
-        meter.append(label);
-		$("#time").html("4711");
-      });
+  meter.html(""); 
+  meter.append(fill);
+  meter.append(label);
+});
 
-	$(function(){ 
-		$("#time").html("4711");
-		$("#context_started").html("47:11");
-	})
+$(function()
+{ 
+  $("#time").html("4711");
+  $("#context_started").html("47:11");
+})
 	
-     var myAjaxErrorHandler = function(xhr, errmsg, err)
-     {
-        alert("Ajax failed: " + errmsg + " : " + err);
-        clearInterval(glob_lp);
-     }
+var myAjaxErrorHandler = function(xhr, errmsg, err)
+{
+  alert("Ajax failed: " + errmsg + " : " + err);
+  clearInterval(glob_lp);
+}
      
-     $.ajaxSetup({ error: myAjaxErrorHandler });
-	var lpReq = function() 
-	{
-		$.getJSON("/LP", function(data)
-		{
-			for (name in data)
-			{
-				$("." + name).each(function(i) {
-				  $(this).html(data[name]);
-				});
-			}
-		});
-	}
+$.ajaxSetup({ error: myAjaxErrorHandler });
+function lpReq() 
+{
+  $.getJSON("/LP", function(data)
+  {
+    var name;
+    for (name in data)
+    {
+      $("." + name).each(function(i) 
+      {
+        $(this).html(data[name]);
+      });
+    }
+    
+    glob_lp = setTimeout(lpReq, 500);
+  });
+}
 
-	$(function(){ 
-		// starte alle 2 Sekunden einen Polling-Request
-		glob_lp = setInterval("lpReq()", 2000);
-      //lpReq();
-	})
-
-	//})
+$(function()
+{ 
+  // starte alle 2 Sekunden einen Polling-Request
+  glob_lp = setTimeout(lpReq, 500);
+})
