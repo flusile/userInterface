bba4a518c5e6b76481a3ca6363cab6534f444628
Ulf Michaelis <ulf@vdr>
2011-10-15 17:52:40 +0200

    Umbau von canvas auf svg ist abgeschlossen. Nun folgt das Einrichten der Event-Handler.

--- src/diagramm.js
+++ src/diagramm.js
@@ -1,27 +1,152 @@
 
+function mLine(id, x1, y1, x2, y2, stroke, strokewidth)
+{
+  var obj = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  obj.setAttribute("id",           id); 
+  obj.setAttribute("stroke",       stroke);
+  obj.setAttribute("stroke-width", strokewidth);
+  obj.setAttribute("x1",           x1);
+  obj.setAttribute("y1",           y1);
+  obj.setAttribute("x2",           x2);
+  obj.setAttribute("y2",           y2);
+  return obj;
+}
+
+function msLine(id, x, y, len, stroke, strokewidth)
+{
+  return mLine(id, x, y, x, y+len, stroke, strokewidth);
+}
+
+function mwLine(id, x, y, len, stroke, strokewidth)
+{
+  return mLine(id, x, y, x+len, y, stroke, strokewidth);
+}
+
+function mTempLine(id, x, y, len)
+{
+  return mwLine(id, x, y, len, "rgb(0,200,0)", 3);
+}
+
+function mRect(id, x, y, xlen, ylen, colour)
+{
+  var obj = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+  obj.setAttribute("id",           id); 
+  obj.setAttribute("stroke",       colour);
+  obj.setAttribute("fill",         colour);
+  obj.setAttribute("x",            x);
+  obj.setAttribute("y",            y);
+  obj.setAttribute("width",        xlen);
+  obj.setAttribute("height",       ylen);
+  return obj;
+}
+
+
+function mSquare(id, x, y, len, colour)
+{
+  return mRect(id, x, y, len, len, colour);
+}
+
+function mCircle(id, x, y, radius, colour)
+{
+  var obj = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  obj.setAttribute("id",           id); 
+  obj.setAttribute("stroke",       colour);
+  obj.setAttribute("fill",         colour);
+  obj.setAttribute("cx",           x);
+  obj.setAttribute("cy",           y);
+  obj.setAttribute("r",            radius);
+  return obj;
+}
+
+function addTempLine(svg, id, x, y, len)
+{
+  svg.append(mwLine("templine_" + id, x, y, len, "rgb(0,200,0)", 3));
+  svg.append(mCircle("temp_pt_" + id, x, y, 6, "rgb(0,0,200)"));
+}
+
+function addTimeLine(svg, id, x, y, len)
+{
+  svg.append(msLine("timeline_" + id, x, y, len, "rgb(0,200,0)", 3));
+}
+
+
 $(function()
 {
-  var canvas = $("#diagramm_1");
-  var kx     = $("#x");
-  var ky     = $("#y");
-  var mud    = $("#mud");
-  var active = false;
-  var xxx    = canvas.offset();
-  var col    = Math.ceil(xxx.left);
-  var cot    = Math.ceil(xxx.top);
+  var kx      = $("#x");
+  var ky      = $("#y");
+  var mud     = $("#mud");
+  var redline = $("#redline");
+  var bluline = $("#blueline");
+  var active  = false;
+  var svga    = $("#svg");
+  var xxx     = svga.offset();
+  var col     = Math.ceil(xxx.left);
+  var cot     = Math.ceil(xxx.top);
   xxx.left=col;
   xxx.top=cot;
-  canvas.offset(xxx);
+  svga.offset(xxx);
   
-  canvas.attr("width", 24*30); // max. 1000, Tageszeit
-  canvas.attr("height", 200); // Temperatur
+//  svga.append(mLine("line1", 10, 10, 100, 100, "black", 5));
+//  svga.append(mLine("line2", 10, 20, 220, 330, "green", 3));
+//  svga.append(mwLine("line3", 0, 0, 24*30, "yellow", 3));
+//  svga.append(msLine("line4", 0, 0, 300, "brown", 3));
+//  svga.append(mCircle("c1", 100, 100, 30, "brown"));
+//  svga.append(mSquare("sq1", 200, 100, 30, "brown"));
 
-  var ctx = document.getElementById("diagramm_1").getContext("2d"); 
-  
   var data = '[{"ts":0,"temp":10},{"ts":600,"temp":30},{"ts":1320,"temp":20}]';
-  
   var va = jQuery.parseJSON(data);
+  // Hintergrund malen
+  svga.append(mRect("ground", 0, 0, 24*30, 300, "rgb(200,200,200)"));
 
+  // 1000/24 == 41,666
+  // Koordinatensystem zeichnen
+  svga.append(mwLine("koord_h", 0, 300, 24*30, "rgb(200,0,0)", 3));
+  svga.append(msLine("koord_v", 0, 0, 300, "rgb(200,0,0)", 3));
+
+  var x=0; // zeit
+  var y=0; // Temperatur in Px von canvas.height herunter
+  for (idx in va)
+  {
+    var e = va[idx]
+    var str = e.ts + " " + e.temp;
+    $("#hilfe").html(str);
+    var nx=e.ts/2;	
+    var ny=300 - e.temp*5;
+    if (nx != 0)
+    {
+      // 1. Waagerecht - die Temperatur-Linie
+      addTempLine(svga, idx-1, x, y, nx-x);
+      
+      // 1. Senkrecht - die Zeit-Linie
+      addTimeLine(svga, idx-1, nx, y, ny-y); 
+    }
+
+    x=nx;
+    y=ny;
+  }
+  
+  // zum schluss die letzte waagerechte Temperatur-Linie
+  addTempLine(svga, idx, x, y, 24*30-x);
+  
+/*
+  redline.mousemove(function(e)
+  {
+    var x = e.pageX - col;
+    var y = e.pageY - cot;
+    kx.html(x);
+    ky.html(y);
+    mud.html("redline");
+  });
+  
+  bluline.mousemove(function(e)
+  {
+    var x = e.pageX - col;
+    var y = e.pageY - cot;
+    kx.html(x);
+    ky.html(y);
+    mud.html("blue line");
+  });
+  
   //$("#hilfe").html(str);
   canvas.mousemove(function(e)
   { // früher: clientX/Y
@@ -48,57 +173,10 @@ $(function()
   {
     active = false; 
   });
-  
+  */
   kx.html(77);
   ky.html(88);
   mud.html("dunno");
-  
-  // Hintergrund malen
-  ctx.fillStyle = "rgb(200,200,200)";  
-  ctx.fillRect(0, 0, canvas.attr("width"), canvas.attr("height"));
-
-  // 1000/24 == 41,666
-  // Koordinatensystem zeichnen
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect(0, 199, 24*60, 1);
-  ctx.fillRect(0, 0, 1, 200);
-  
-
-  var x=0; // zeit
-  var y=0; // Temperatur in Px von canvas.height herunter
-  for (idx in va)
-  {
-    var e = va[idx]
-    var str = e.ts + " " + e.temp;
-    $("#hilfe").html(str);
-    var nx=e.ts/2;	
-    var ny=canvas.attr("height") - e.temp*5;
-    if (nx != 0)
-    {
-      // 1. Waagerecht
-      ctx.fillStyle = "rgb(0,200,0)";
-      ctx.fillRect(x, y-1, nx-x, 3);
-
-      ctx.fillStyle = "rgb(0,0,200)";
-      ctx.fillRect(x-3, y-3, 7, 7);
-      
-      // Male Linie von x/y nach nx/ny
-      // 1. Senkrecht
-      ctx.fillStyle = "rgb(0,200,0)";
-      ctx.fillRect(nx-1, y, 3, ny-y);    
-    }
-
-    x=nx;
-    y=ny;
-  }
-  
-  // zum schluss die letzte waagerechte
-  nx=canvas.attr("width");
-  ctx.fillStyle = "rgb(0,200,0)";
-  ctx.fillRect(x, y-1, nx-x, 3);
-
-  ctx.fillStyle = "rgb(0,0,200)";
-  ctx.fillRect(x-3, y-3, 7, 7);
 })
 
 
